7a684eb3de22e2b56683112c8b9078489e56d916
sravanpabolu <TCS@geethikakoneru.local>
2017-03-16 15:12:03 +0530

    Initial Commit

---
+++ FlickrCollectionPOC/FlickrCell.swift
@@ -0,0 +1,15 @@
+//
+//  FlickrCell.swift
+//  FlickrCollectionPOC
+//
+//  Created by TCS on 16/03/17.
+//
+//
+
+import Foundation
+import UIKit
+
+class FlickrCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
